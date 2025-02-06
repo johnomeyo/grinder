cad8c83c184668c9daa4a5a5e5c441b1ee004666
John Omeyo <johnomeyo@gmail.com>
2025-02-06 17:55:38 +0300

    added relevant images in the onboarding pages

--- lib/constants/onboarding_data.dart
+++ lib/constants/onboarding_data.dart
@@ -1,27 +1,27 @@
 final List<Map<String, String>> onboardingData = [
   {
-    "image": "assets/glogo.png", 
+    "image": "assets/welcome.png",
     "text": "Welcome to Grinder!",
     "title": "Track Your Job Applications Effortlessly",
     "subtitle":
         "Keep all your job applications in one place, monitor progress, and never miss an update.",
   },
   {
-    "image": "assets/track_progress.png", 
+    "image": "assets/organised.png",
     "text": "Stay Organized and Informed",
     "title": "Know Where You Stand",
     "subtitle":
         "Easily categorize applications, track responses, and get insights on your job search journey.",
   },
   {
-    "image": "assets/analytics.png", 
+    "image": "assets/graph.png",
     "text": "Optimize Your Job Search",
     "title": "Analyze Your Success Rate",
     "subtitle":
         "Get statistics on your applications, understand what works, and improve your chances of landing a job.",
   },
   {
-    "image": "assets/get_started.png", 
+    "image": "assets/get_started.png",
     "text": "Let’s Get Started!",
     "title": "Take Control of Your Career",
     "subtitle":
